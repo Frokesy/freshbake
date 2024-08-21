07c70fe2d6b457af7ed8b8bb699f8593d72a1f1d
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-08-21 17:31:46 +0100

    enabled PWA for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 function App() {
 
   return (
     <>
-      <h2 className="text-3xl text-red-500">Fresh bake initialized</h2>
+        <h2 className="text-3xl text-red-500">Fresh bake initialized</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
