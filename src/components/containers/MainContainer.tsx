22d664d1b18aec3d14209d40c7ed212fad36b2f6
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-08-23 04:47:19 +0100

    imported topnav icons

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -1,16 +1,19 @@
 import { FC, ReactNode } from "react";
 import PageTransition from "../defaults/PageTransition";
 import BottomNav from "../defaults/BottomNav";
+import TopNav from "../defaults/TopNav";
 
 interface MainContainerProps {
   children: ReactNode;
   active: string;
 }
 const MainContainer: FC<MainContainerProps> = ({ children, active }) => {
-
   return (
     <div className="bg-[#fffaed] min-h-screen lg:w-[480px] mx-auto">
-      <PageTransition active={active}>{children}</PageTransition>
+      <TopNav />
+      <PageTransition active={active}>
+        <div className="pt-10">{children}</div>
+      </PageTransition>
       <BottomNav active={active} />
     </div>
   );
---
+++ src/components/defaults/TopNav.tsx
@@ -0,0 +1,9 @@
+const TopNav = () => {
+  return (
+    <div className="fixed top-0 w-[100%] z-10">
+      <h2>Top Nav</h2>
+    </div>
+  )
+}
+
+export default TopNav
