5b48720357abdc212663381b20227ce60197626f
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-08-23 03:46:18 +0100

    configured react-router-dom to handle page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { AnimatePresence } from "framer-motion";
 import Onboarding from "./pages/onboarding";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <Onboarding /> },
+    { path: "/home", element: <Home /> }
+  ])
 
   return (
-    <>
-      <Onboarding />
-    </>
+    <AnimatePresence>
+      <RouterProvider router={router} />
+    </AnimatePresence>
   );
 }
 
