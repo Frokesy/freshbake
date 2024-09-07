d3350956dbdeef3e01d32e2166691776524bcd6d
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-09-07 10:34:17 +0100

    wrote logic to fetch all users

--- src/pages/admin/UserInfo.tsx
+++ src/pages/admin/UserInfo.tsx
@@ -1,8 +1,26 @@
 import { NavLink } from "react-router-dom";
 import AdminContainer from "../../components/containers/AdminContainer";
 import { ArrowLeft } from "../../components/icons";
+import { useEffect, useState } from "react";
+import { UserDataProps } from "../home";
+import { supabase } from "../../../utils/supabaseClient";
 
 const UserInfo = () => {
+  const [users, setUsers] = useState<UserDataProps[]>();
+  useEffect(() => {
+    const getUsers = async () => {
+      const { data, error } = await supabase.from("users").select("*");
+      if (!error) {
+        setUsers(data);
+      } else {
+        console.log(error);
+      }
+    };
+    getUsers();
+  }, []);
+
+  console.log(users);
+
   return (
     <AdminContainer active="Account">
       <div className="flex items-center space-x-4 px-4 pt-10">
@@ -18,27 +36,18 @@ const UserInfo = () => {
       </div>
 
       <div className="mt-6 text-[14px] space-y-3">
-        <div className="flex justify-between items-center border border-[#ccc] p-4">
-          <div className="">
-            <h2>Jackson Adeolu</h2>
-            <p>jacksonadeolu21@gmail.com</p>
+        {users?.map((user) => (
+          <div
+            key={user.id}
+            className="flex justify-between items-center border border-[#ccc] p-4"
+          >
+            <div className="">
+              <h2>{user.firstname} {user.lastname}</h2>
+              <p>{user.email}</p>
+            </div>
+            <p>{user.phone}</p>
           </div>
-          <p>+432-657-3953</p>
-        </div>
-        <div className="flex justify-between items-center border border-[#ccc] p-4">
-          <div className="">
-            <h2>Jackson Adeolu</h2>
-            <p>jacksonadeolu21@gmail.com</p>
-          </div>
-          <p>+432-657-3953</p>
-        </div>
-        <div className="flex justify-between items-center border border-[#ccc] p-4">
-          <div className="">
-            <h2>Jackson Adeolu</h2>
-            <p>jacksonadeolu21@gmail.com</p>
-          </div>
-          <p>+432-657-3953</p>
-        </div>
+        ))}
       </div>
     </AdminContainer>
   );
