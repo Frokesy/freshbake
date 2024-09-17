766b9b287633480cd0eb1f0c42b5baed7d44a3eb
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-09-17 22:39:39 +0100

    wrote logic to fetch messages

--- src/pages/customer-service/index.tsx
+++ src/pages/customer-service/index.tsx
@@ -1,34 +1,77 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NavLink } from "react-router-dom";
 import { ArrowLeft, CustomerAvatar, SendIcon } from "../../components/icons";
 import { useEffect, useState, useRef } from "react";
 import { UserDataProps } from "../home";
 import { supabase } from "../../../utils/supabaseClient";
 
+interface MessageProps {
+  id?: string;
+  timestamp: string | undefined;
+  sender: string | undefined;
+  name: string | undefined;
+  message: string | undefined;
+}
+
 const LiveSupport = () => {
   const [userData, setUserData] = useState<UserDataProps>();
   const [messageText, setMessageText] = useState<string>("");
-  const [messages, setMessages] = useState<any[]>([]);
+  const [messages, setMessages] = useState<MessageProps[]>([]);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const hasRun = useRef(false);
+
+  const getUser = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user) {
+      const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("userId", user.id);
+      if (!error && data.length > 0) {
+        const fetchedUserData = data[0];
+        setUserData(fetchedUserData);
+
+        const adminMsg = [
+          {
+            sender: "admin",
+            name: `${fetchedUserData.firstname} ${fetchedUserData.lastname}`,
+            message: `Hello, ${fetchedUserData.firstname}! \n How can we assist you today?`,
+            timestamp: new Date().toISOString(),
+          },
+        ];
+        setMessages((prevMessages) => [
+          ...(adminMsg as MessageProps[]),
+          ...prevMessages,
+        ]);
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
+  const fetchMessages = async () => {
+    const { data, error } = await supabase
+      .from("messages")
+      .select("*")
+      .order("timestamp", { ascending: true });
+
+    if (error) {
+      console.error("Error fetching messages:", error);
+    } else if (data) {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        ...(data as MessageProps[]),
+      ]);
+    }
+  };
 
   useEffect(() => {
-    const getUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-      if (user) {
-        const { data, error } = await supabase
-          .from("users")
-          .select("*")
-          .eq("userId", user.id);
-        if (!error) {
-          data.map((data) => setUserData(data));
-        } else {
-          console.log(error);
-        }
-      }
-    };
-    getUser();
+    if (!hasRun.current) {
+      hasRun.current = true;
+      getUser();
+      fetchMessages();
+    }
   }, []);
 
   const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -44,7 +87,7 @@ const LiveSupport = () => {
     if (messageText.trim()) {
       const newMessage = {
         sender: userData?.userId,
-        name: userData?.firstname + ' ' + userData?.lastname,
+        name: `${userData?.firstname} ${userData?.lastname}`,
         message: messageText,
         timestamp: new Date().toISOString(),
       };
@@ -56,7 +99,7 @@ const LiveSupport = () => {
       if (error) {
         console.error("Error sending message:", error);
       } else {
-        console.log(data)
+        console.log(data);
         setMessages((prevMessages) => [...prevMessages, newMessage]);
         setMessageText("");
       }
@@ -65,7 +108,6 @@ const LiveSupport = () => {
 
   return (
     <div>
-      {/* Header */}
       <div className="flex items-center space-x-4 px-4 pt-10 fixed top-0 bg-[#fff] w-[100%] pb-3">
         <div className="flex">
           <NavLink to="/home" className="bg-[#d9d9d9] p-1.5 rounded-full">
@@ -78,7 +120,7 @@ const LiveSupport = () => {
       <div className="msg-body px-4 py-[15vh] text-sm flex flex-col space-y-3">
         {messages.map((message) => (
           <div
-            key={message.id}
+            key={message.timestamp}
             className={`flex items-start ${
               message.sender === userData?.userId
                 ? "justify-end"
@@ -110,7 +152,6 @@ const LiveSupport = () => {
         ))}
       </div>
 
-      {/* Message Input */}
       <div className="fixed bottom-0 pb-6 bg-[#fff] pt-2 w-[100%] px-4">
         <div className="flex justify-between px-4 py-3 rounded-lg items-center bg-[#e4e4e4]">
           <textarea
@@ -120,7 +161,7 @@ const LiveSupport = () => {
             placeholder="write your message..."
             className="w-[90%] outline-none border-none bg-[#e4e4e4] text-[14px] placeholder:text-[14px] placeholder:text-[#333] resize-none"
             rows={1}
-            style={{ overflow: "hidden" }} // Prevent scrollbar
+            style={{ overflow: "hidden" }}
           />
           <div onClick={sendMessage} className="cursor-pointer">
             <SendIcon />
