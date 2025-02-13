b0629d66952a7a00210fd36e8db13add46195fb6
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-02-13 06:12:24 +0100

    migrated login func from supabase to pocketbase

--- utils/loginService.ts
+++ utils/loginService.ts
@@ -1,36 +1,32 @@
 import { useNavigate } from "react-router-dom";
-import { supabase } from "./supabaseClient";
 import { Bounce, toast } from "react-toastify";
+import { pb } from "./pocketbaseClient";
 
 export async function handleLogin(
-    validateField: (value: string) => boolean,
-    user: { input: string, password: string},
-    setError: React.Dispatch<
-    React.SetStateAction<{ input: string; password: string; }>
+  validateField: (value: string) => boolean,
+  user: { input: string; password: string },
+  setError: React.Dispatch<
+    React.SetStateAction<{ input: string; password: string }>
   >,
   setLoading: React.Dispatch<React.SetStateAction<boolean>>,
   navigate: ReturnType<typeof useNavigate>,
   keepLoggedIn: boolean
 ) {
-    const isInputValid = validateField(user.input);
-    const isPasswordValid = validateField(user.password);
-    setLoading(true)
-  
-    setError({
-      input: isInputValid ? "" : "Field is required",
-      password: isPasswordValid ? "" : "Field is required",
-    });
+  const isInputValid = validateField(user.input);
+  const isPasswordValid = validateField(user.password);
+  setLoading(true);
 
-    if (isInputValid && isPasswordValid) {
-      try {
-        const { data, error } = await supabase.auth.signInWithPassword({
-          email: user.input,
-          password: user.password
-        });
-        if (error) {
-          setLoading(false);
-          throw error.message
-        }
+  setError({
+    input: isInputValid ? "" : "Field is required",
+    password: isPasswordValid ? "" : "Field is required",
+  });
+
+  if (isInputValid && isPasswordValid) {
+    try {
+      const authData = await pb
+        .collection("users")
+        .authWithPassword(user.input, user.password);
+      if (authData) {
         toast.success(`Welcome back!`, {
           position: "top-right",
           theme: "light",
@@ -41,35 +37,37 @@ export async function handleLogin(
           transition: Bounce,
         });
         if (keepLoggedIn) {
-          localStorage.setItem("authToken", data.session.access_token);
+          localStorage.setItem("authToken", authData.token);
         } else {
-          sessionStorage.setItem("authToken", data.session.access_token);
+          sessionStorage.setItem("authToken", authData.token);
         }
         setLoading(false);
         setTimeout(() => {
           navigate("/home");
         }, 2200);
-      } catch (error) {
-        toast.error(error as string, {
-          position: "top-right",
-          theme: "light",
-          autoClose: 2000,
-          hideProgressBar: true,
-          draggable: true,
-        });
-        setLoading(false);
       }
-    } else {
+    } catch (error) {
+      toast.error("Invalid login credentials", {
+        position: "top-right",
+        theme: "light",
+        autoClose: 2000,
+        hideProgressBar: true,
+        draggable: true,
+      });
       setLoading(false);
-      if (!isInputValid) {
-        setTimeout(() => {
-          setError((prevState) => ({ ...prevState, input: "" }));
-        }, 3000);
-      }
-      if (!isPasswordValid) {
-        setTimeout(() => {
-          setError((prevState) => ({ ...prevState, password: "" }));
-        }, 3000);
-      }
+      console.error(error);
     }
+  } else {
+    setLoading(false);
+    if (!isInputValid) {
+      setTimeout(() => {
+        setError((prevState) => ({ ...prevState, input: "" }));
+      }, 3000);
+    }
+    if (!isPasswordValid) {
+      setTimeout(() => {
+        setError((prevState) => ({ ...prevState, password: "" }));
+      }, 3000);
+    }
+  }
 }
