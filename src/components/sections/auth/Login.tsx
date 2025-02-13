ea84eee13ca59cb0fa63bed8593c791d55488e4f
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-02-13 05:40:42 +0100

    initialized pocketbase instance

--- src/components/sections/auth/Login.tsx
+++ src/components/sections/auth/Login.tsx
@@ -6,6 +6,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import Spinner from "../../defaults/Spinner";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { pb } from "../../../../utils/pocketbaseClient";
 
 export interface LoginProps {
   setActiveScreen: React.Dispatch<React.SetStateAction<string>>;
@@ -42,6 +43,8 @@ const Login: FC<LoginProps> = ({ setActiveScreen }) => {
       keepLoggedIn
     );
   };
+
+  console.log(pb);
 
   useEffect(() => {
     const token =
