d34dbf4c5dc4585af81bc0a414806c9b08d5ffa8
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-03-17 18:33:34 +0100

    changed pocketbase url

--- utils/pocketbaseClient.ts
+++ utils/pocketbaseClient.ts
@@ -1,5 +1,3 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('http://127.0.0.1:8090/');
-
-// frehsbake-pb-server.railway.internal
+export const pb = new PocketBase('https://frehsbake-pb-server-production.up.railway.app/');
